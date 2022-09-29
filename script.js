cec6fd2e2dc1177ce349b30d7d6b2c30535ed5de
duffaldri <mdaffaaldriantama@gmail.com>
2022-09-29 20:10:38 +0700

    Added hometown, local foods, and tourist places pages. Fixed navbar minor bug.

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ let header = document.querySelector(".navbar");
 
 window.addEventListener("scroll", function () {
     let value = window.scrollY;
-    header.classList.toggle("scroll", window.scrollY > 1);
+    header.classList.toggle("scrolled", window.scrollY > 1);
 });
+
+// $(function () {
+//     $(document).scroll(function () {
+//         var $nav = $(".navbar-sticky-top");
+//         $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+//     });
+// });
